0d4942150e15f1e0277e9774520b0818ec58b1b4
garce07 <guillermoacgx@gmail.com>
2014-06-04 20:36:50 -0600

    PREUBA CON ESTEBAN 2

--- SCRUMTEC/Form1.Designer.cs
+++ SCRUMTEC/Form1.Designer.cs
@@ -37,7 +37,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(184, 143);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Soy Esteban";
+            this.button1.Text = "ola k ace? probando o k ace?";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
