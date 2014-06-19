41df0e8894437b7ab14822958b0e9f4137f42cb4
AdrianMarin <adrianjmch@gmail.com>
2014-06-19 10:37:37 -0600

    InsertarUserStory

--- SCRUMTEC/ConexionMetodos.cs
+++ SCRUMTEC/ConexionMetodos.cs
@@ -477,7 +477,7 @@ namespace SCRUMTEC
 
 
 
-        //-----------------------
+        //-----------------------------------------------------------------------------------------
         public static int insertarProyecto(String nombre_proyecto, String descripcion_proyecto)
         {
 
@@ -488,6 +488,27 @@ namespace SCRUMTEC
                 Comando.CommandType = CommandType.StoredProcedure;
                 Comando.Parameters.AddWithValue("@nombre", nombre_proyecto);
                 Comando.Parameters.AddWithValue("@descripcion", descripcion_proyecto);
+                resultado = Comando.ExecuteNonQuery();
+                Conn.Close();
+                return resultado;
+            }
+        }
+
+
+        //-----------------------------------------------------------------------------------
+        public static int insertarUserStory(String nombre_user_story, String descripcion_user_story, String prioridad, int id_proyecto, int id_sprint)
+        {
+            int resultado;
+            using (SqlConnection Conn = Conexion.ObtenerConexion())
+            {
+                SqlCommand Comando = new SqlCommand("insertarUserStory", Conn);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.AddWithValue("@nombre", nombre_user_story);
+                // Comando.Parameters.AddWithValue("@descripcion", descripcion_user_story);  //no esta en la base de datos
+                Comando.Parameters.AddWithValue("@prioridad", prioridad);
+                Comando.Parameters.AddWithValue("@FKProyecto", id_proyecto);
+                Comando.Parameters.AddWithValue("@FKSprint", id_sprint);
+
                 resultado = Comando.ExecuteNonQuery();
                 Conn.Close();
                 return resultado;
