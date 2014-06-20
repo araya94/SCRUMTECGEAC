8bbf5693d2f5d572fcedf357affd2a18b51a2894
araya94 <cristian.araya@hotmail.com>
2014-06-19 20:21:11 -0600

    VentanaCrearBurnDownHorasConPrimeraLinea

--- SCRUMTEC/procedimientos.sql
+++ SCRUMTEC/procedimientos.sql
@@ -1,31 +1,47 @@
 /*create procedure buscarRoles
 as
 begin
-	begin try
-			Select * from Rol
-	end try
-	begin catch
-		select ERROR_NUMBER() as ErrorNumber;
-		return -1;
-	end catch
+begin try
+Select * from Rol
+end try
+begin catch
+select ERROR_NUMBER() as ErrorNumber;
+return -1;
+end catch
+end
+
+create procedure SP_TOTALHORAS
+@proyecto int
+as
+begin
+begin try
+select sum(T.DuracionEstimada)
+from Tarea T, UserStory US
+where T.FKUserStory = US.id
+and US.FKProyecto = @proyecto
+end try
+begin catch
+select ERROR_NUMBER() as ErrorNumber;
+return -1;
+end catch
 end
 
 create procedure SP_INSERTAR_ASOCIACIONUSER
 @userS int,
 @usuario int
 as
-begin 
+begin
 begin try
-		begin transaction
-			insert User_Usuario(FKUserStory,FKUsuarioP)
-			values(@userS,@usuario)
-		commit transaction
-	end try
-	begin catch
-		select ERROR_NUMBER() as ErrorNumber;
-		return -1;
-		rollback transaction
-	end catch 
+begin transaction
+insert User_Usuario(FKUserStory,FKUsuarioP)
+values(@userS,@usuario)
+commit transaction
+end try
+begin catch
+select ERROR_NUMBER() as ErrorNumber;
+return -1;
+rollback transaction
+end catch
 end
 
 create procedure insertarUsuario
@@ -36,21 +52,21 @@ create procedure insertarUsuario
 @proyecto int,
 @rol int
 as
-begin 
+begin
 begin try
-		begin transaction
-			insert Usuario(FKRol,nombre,email,usuario,contrasena)
-			values(@rol,@nombre,@email,@usuario,@contrasena)
-			
-			insert Usuario_Proyecto(FKUsuario,FKProyecto)
-			values((select max(Id) from Usuario),@proyecto)
-		commit transaction
-	end try
-	begin catch
-		select ERROR_NUMBER() as ErrorNumber;
-		return -1;
-		rollback transaction
-	end catch 
+begin transaction
+insert Usuario(FKRol,nombre,email,usuario,contrasena)
+values(@rol,@nombre,@email,@usuario,@contrasena)
+
+insert Usuario_Proyecto(FKUsuario,FKProyecto)
+values((select max(Id) from Usuario),@proyecto)
+commit transaction
+end try
+begin catch
+select ERROR_NUMBER() as ErrorNumber;
+return -1;
+rollback transaction
+end catch
 end
 
 create procedure autentificacion
@@ -59,14 +75,14 @@ create procedure autentificacion
 as
 begin
 begin try
-		Select * From Usuario 
-		where usuario like @usuario +'%'
-		and contrasena like @contraseña +'%'
+Select * From Usuario
+where usuario like @usuario +'%'
+and contrasena like @contraseña +'%'
 end try
-	begin catch
-		select ERROR_NUMBER() as ErrorNumber;
-		return -1;
-	end catch 
+begin catch
+select ERROR_NUMBER() as ErrorNumber;
+return -1;
+end catch
 end
 
 create procedure SP_INSERTAR_SPRINT
@@ -74,37 +90,37 @@ create procedure SP_INSERTAR_SPRINT
 @descripcion text,
 @releas int
 as
-begin 
+begin
 begin try
-		begin transaction
-			insert Sprint(Nombre,Descripcion)
-			values(@nombre,@descripcion)
-			
-			insert Sprint_Release(FKRelease,FKSprint)
-			values(@releas,(select max(Id) from Sprint))
-			
-			return (select max(Id) from Sprint) /*Retorna el id del sprint creado actual */
-		commit transaction
-	end try
-	begin catch
-		select ERROR_NUMBER() as ErrorNumber;
-		return -1;
-		rollback transaction
-	end catch
+begin transaction
+insert Sprint(Nombre,Descripcion)
+values(@nombre,@descripcion)
+
+insert Sprint_Release(FKRelease,FKSprint)
+values(@releas,(select max(Id) from Sprint))
+
+return (select max(Id) from Sprint) /*Retorna el id del sprint creado actual */
+commit transaction
+end try
+begin catch
+select ERROR_NUMBER() as ErrorNumber;
+return -1;
+rollback transaction
+end catch
 end
 
 create procedure SP_BUSCARNOMBRE_PROYECTO
 @proyecto int
 as
 begin
-	begin try
-			Select * from Proyecto P
-			where  @proyecto = P.id 
-	end try
-	begin catch
-		select ERROR_NUMBER() as ErrorNumber;
-		return -1;
-	end catch
+begin try
+Select * from Proyecto P
+where  @proyecto = P.id
+end try
+begin catch
+select ERROR_NUMBER() as ErrorNumber;
+return -1;
+end catch
 end
 
 create procedure SP_ACTUALIZARTIEMPOSPRINT_PROYECTO
@@ -112,135 +128,135 @@ create procedure SP_ACTUALIZARTIEMPOSPRINT_PROYECTO
 @proyecto int
 as
 begin
-	begin try
-		update Proyecto
-		set DuracionSprint = @tiempo
-		where id = @proyecto
-	end try
-	begin catch
-		select ERROR_NUMBER() as ErrorNumber;
-		return -1;
-	end catch
+begin try
+update Proyecto
+set DuracionSprint = @tiempo
+where id = @proyecto
+end try
+begin catch
+select ERROR_NUMBER() as ErrorNumber;
+return -1;
+end catch
 end
 
 create procedure SP_BUSCARESFUERZO_TAREA
 @tarea int
 as
 begin
-	begin try
-			Select T.id,T.EsfuerzoInvertido from Tarea T
-			where  @tarea = T.id 
-	end try
-	begin catch
-		select ERROR_NUMBER() as ErrorNumber;
-		return -1;
-	end catch
+begin try
+Select T.id,T.EsfuerzoInvertido from Tarea T
+where  @tarea = T.id
+end try
+begin catch
+select ERROR_NUMBER() as ErrorNumber;
+return -1;
+end catch
 end
 
 create procedure SP_ACTUALIZARESFUERZO_TAREA
 @tiempo int,
 @sprint int
 as
-begin 
-	begin try
-		update Tarea
-		set EsfuerzoInvertido = @tiempo
-		where id = @sprint
-	end try
-	begin catch
-		select ERROR_NUMBER() as ErrorNumber;
-		return -1;
-		rollback transaction
-	end catch
+begin
+begin try
+update Tarea
+set EsfuerzoInvertido = @tiempo
+where id = @sprint
+end try
+begin catch
+select ERROR_NUMBER() as ErrorNumber;
+return -1;
+rollback transaction
+end catch
 end
 
 create procedure SP_ACTUALIZARREVIEW_SPRINT
 @descrip text,
 @idSprint int
 as
-begin 
-	begin try
-		update Sprint
-		set SprintReview = @descrip
-		where id = @idSprint
-	end try
-	begin catch
-		select ERROR_NUMBER() as ErrorNumber;
-		return -1;
-		rollback transaction
-	end catch
+begin
+begin try
+update Sprint
+set SprintReview = @descrip
+where id = @idSprint
+end try
+begin catch
+select ERROR_NUMBER() as ErrorNumber;
+return -1;
+rollback transaction
+end catch
 end
 
 create procedure SP_ACTUALIZARFKSPRI_USER
 @sprint int,
 @user int
 as
-begin 
-	begin try
-		update UserStory
-		set FKSprint = @sprint
-		where id = @user
-	end try
-	begin catch
-		select ERROR_NUMBER() as ErrorNumber;
-		return -1;
-		rollback transaction
-	end catch
+begin
+begin try
+update UserStory
+set FKSprint = @sprint
+where id = @user
+end try
+begin catch
+select ERROR_NUMBER() as ErrorNumber;
+return -1;
+rollback transaction
+end catch
 end
 
 create procedure SP_BUSCAR_USER_SPRIN
 @sprint int
 as
 begin
-	begin try
-			Select T.Id,T.Nombre from UserStory T, Proyecto P, Sprint_Release SR,Release_Proyecto RP
-			where SR.FKSprint = @sprint 
-			and SR.FKRelease = RP.FKRelease
-			and RP.FKProyecto = T.FKProyecto
-			and T.FKSprint = null
-	end try
-	begin catch
-		select ERROR_NUMBER() as ErrorNumber;
-		return -1;
-	end catch
+begin try
+Select T.Id,T.Nombre from UserStory T, Proyecto P, Sprint_Release SR,Release_Proyecto RP
+where SR.FKSprint = @sprint
+and SR.FKRelease = RP.FKRelease
+and RP.FKProyecto = T.FKProyecto
+and T.FKSprint = null
+end try
+begin catch
+select ERROR_NUMBER() as ErrorNumber;
+return -1;
+end catch
 end
 
 create procedure CargarProyectosUsuario
-                @IDUsuario int
-                as
-                begin
-                begin try
-		            begin transaction
+@IDUsuario int
+as
+begin
+begin try
+begin transaction
 
-			            Select * from dbo.Proyecto P
-				            inner join dbo.Usuario_Proyecto UP on UP.FKUsuario = @IDUsuario
-					            where P.id = UP.FKProyecto
-		            commit transaction
-	            end try
-	            begin catch
-		            select ERROR_NUMBER() as ErrorNumber;
-		            return -1;
-		            rollback transaction
-	            end catch 
-            end
+Select * from dbo.Proyecto P
+inner join dbo.Usuario_Proyecto UP on UP.FKUsuario = @IDUsuario
+where P.id = UP.FKProyecto
+commit transaction
+end try
+begin catch
+select ERROR_NUMBER() as ErrorNumber;
+return -1;
+rollback transaction
+end catch
+end
 
 create procedure insertarProyecto
 @nombre varchar(100),
 @descripcion varchar(100),
 @id_usuario int
 as
-BEGIN 
-   IF NOT EXISTS (SELECT nombre FROM Proyecto
-                   WHERE nombre = @nombre)
-   BEGIN
-       insert Proyecto(nombre,descripcion)
-	   values(@nombre,@descripcion)
+BEGIN
+IF NOT EXISTS (SELECT nombre FROM Proyecto
+WHERE nombre = @nombre)
+BEGIN
+insert Proyecto(nombre,descripcion)
+values(@nombre,@descripcion)
 
-	   insert Usuario_Proyecto(FKUsuario, FKProyecto)
-	   values(@id_usuario,(select max(Id) from Proyecto))
-   END
-   ELSE return -1;
-END 
+insert Usuario_Proyecto(FKUsuario, FKProyecto)
+values(@id_usuario,(select max(Id) from Proyecto))
+END
+ELSE return -1;
+END
 
 
 create procedure insertarUserStory
@@ -249,15 +265,15 @@ create procedure insertarUserStory
 @nombre varchar(100),
 @prioridad varchar(100)
 as
-BEGIN 
-   IF NOT EXISTS (SELECT nombre FROM UserStory
-                   WHERE nombre = @nombre)
-   BEGIN
-       insert UserStory(FKProyecto,FKSprint,nombre,prioridad)
-	   values(@FKProyecto, @FKSprint, @nombre,@prioridad)
-   END
-   ELSE return -1;
-END 
+BEGIN
+IF NOT EXISTS (SELECT nombre FROM UserStory
+WHERE nombre = @nombre)
+BEGIN
+insert UserStory(FKProyecto,FKSprint,nombre,prioridad)
+values(@FKProyecto, @FKSprint, @nombre,@prioridad)
+END
+ELSE return -1;
+END
 */
 
 /**
@@ -267,114 +283,114 @@ create procedure insertarUserStory
 @descripcion varchar(500),
 @prioridad varchar(100)
 as
-BEGIN 
-   IF NOT EXISTS (SELECT nombre FROM UserStory
-                   WHERE nombre = @nombre)
-   BEGIN
-       insert UserStory(FKProyecto,nombre, descripcion ,prioridad)
-	   values(@FKProyecto, @nombre, @descripcion ,@prioridad)
-   END
-   ELSE return -1;
-END 
+BEGIN
+IF NOT EXISTS (SELECT nombre FROM UserStory
+WHERE nombre = @nombre)
+BEGIN
+insert UserStory(FKProyecto,nombre, descripcion ,prioridad)
+values(@FKProyecto, @nombre, @descripcion ,@prioridad)
+END
+ELSE return -1;
+END
 */
 
 /*--------------------------------------------- ESTEBAN -----------------------------------------------------------
 
 create procedure SP_ACTUALIZAR_USERSTORIE
-	 @ID INT, 
-	 @NOMBRE VARCHAR(50),
-	 @PRIORIDAD VARCHAR(500)
+@ID INT,
+@NOMBRE VARCHAR(50),
+@PRIORIDAD VARCHAR(500)
 AS
 
 BEGIN
 BEGIN TRY
-	BEGIN TRANSACTION
-	SET NOCOUNT ON;
+BEGIN TRANSACTION
+SET NOCOUNT ON;
 
-	UPDATE dbo.UserStory  
-	SET  Nombre=@NOMBRE,Prioridad = @PRIORIDAD
-	WHERE id = @ID
-	COMMIT TRANSACTION
+UPDATE dbo.UserStory
+SET  Nombre=@NOMBRE,Prioridad = @PRIORIDAD
+WHERE id = @ID
+COMMIT TRANSACTION
 END TRY
 BEGIN CATCH
-	select ERROR_NUMBER() as ErrorNumber;
-	return -1;
-	rollback transaction
+select ERROR_NUMBER() as ErrorNumber;
+return -1;
+rollback transaction
 END CATCH
 END
 
 create procedure SP_INGRESAR_CRITERIO
 
-	 @FK_USERSTORY INT,
-	 @NOMBRE VARCHAR(500), 
-	 @DESCRIPCION TEXT,
-	 @ESTADO BIT
+@FK_USERSTORY INT,
+@NOMBRE VARCHAR(500),
+@DESCRIPCION TEXT,
+@ESTADO BIT
 
 AS
 BEGIN
 BEGIN TRY
-	BEGIN TRANSACTION
-	
-	SET NOCOUNT ON;
-	
-	
-	INSERT INTO dbo.Criteri VALUES (@FK_USERSTORY,@NOMBRE,@DESCRIPCION,@ESTADO)
-	
-	COMMIT TRANSACTION
+BEGIN TRANSACTION
+
+SET NOCOUNT ON;
+
+
+INSERT INTO dbo.Criteri VALUES (@FK_USERSTORY,@NOMBRE,@DESCRIPCION,@ESTADO)
+
+COMMIT TRANSACTION
 END TRY
 BEGIN CATCH
-	SELECT ERROR_NUMBER() AS ErrorNumber;
-	RETURN -1;
-	ROLLBACK TRANSACTION
+SELECT ERROR_NUMBER() AS ErrorNumber;
+RETURN -1;
+ROLLBACK TRANSACTION
 END CATCH
 END
 
 create procedure SP_INGRESAR_TAREA
 
-	 @FK_USERSTORY INT, 
-	 @NOMBRE VARCHAR(500),
-	 @DESCRIPCION TEXT,
-	 @DURACIONESTIMADA INT,
-	 @DURACIONINVERTIDA INT
+@FK_USERSTORY INT,
+@NOMBRE VARCHAR(500),
+@DESCRIPCION TEXT,
+@DURACIONESTIMADA INT,
+@DURACIONINVERTIDA INT
 
 
 AS
 BEGIN
 BEGIN TRY
-	BEGIN TRANSACTION
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
-	SET NOCOUNT ON;
-	INSERT INTO dbo.Tarea VALUES (@FK_USERSTORY, @NOMBRE, @DESCRIPCION,@DURACIONESTIMADA,@DURACIONINVERTIDA) 
-	COMMIT TRANSACTION
+BEGIN TRANSACTION
+-- SET NOCOUNT ON added to prevent extra result sets from
+-- interfering with SELECT statements.
+SET NOCOUNT ON;
+INSERT INTO dbo.Tarea VALUES (@FK_USERSTORY, @NOMBRE, @DESCRIPCION,@DURACIONESTIMADA,@DURACIONINVERTIDA)
+COMMIT TRANSACTION
 END TRY
 BEGIN CATCH
-	SELECT ERROR_NUMBER() AS ErrorNumber;
-	RETURN -1;
-	ROLLBACK TRANSACTION
+SELECT ERROR_NUMBER() AS ErrorNumber;
+RETURN -1;
+ROLLBACK TRANSACTION
 END CATCH
 END
 
 create procedure SP_OBTENER_USERSORY_x_ID_SPRING
 
-	 @ID_SPRING INT
+@ID_SPRING INT
 
 AS
 BEGIN
 BEGIN TRY
-	BEGIN TRANSACTION
-	
-	SET NOCOUNT ON;
-	
-	SELECT usu.FKSprint,usu.Nombre,usu.Prioridad
-	FROM dbo.UserStory usu 
-	WHERE usu.FKSprint = @ID_SPRING
-	COMMIT TRANSACTION
+BEGIN TRANSACTION
+
+SET NOCOUNT ON;
+
+SELECT usu.FKSprint,usu.Nombre,usu.Prioridad
+FROM dbo.UserStory usu
+WHERE usu.FKSprint = @ID_SPRING
+COMMIT TRANSACTION
 END TRY
 BEGIN CATCH
-	SELECT ERROR_NUMBER() AS ErrorNumber;
-	RETURN -1;
-	ROLLBACK TRANSACTION
+SELECT ERROR_NUMBER() AS ErrorNumber;
+RETURN -1;
+ROLLBACK TRANSACTION
 END CATCH
 END
 
@@ -382,21 +398,21 @@ END
 
 
 create procedure CargarReleases
-                @IDProyecto int
-                as
-                begin
-                begin try
-		            begin transaction
+@IDProyecto int
+as
+begin
+begin try
+begin transaction
 
-			            Select * from dbo.Release R
-				            inner join dbo.Release_Proyecto RP on RP.FKProyecto = @IDProyecto
-					            where R.id = RP.FKRelease
-		            commit transaction
-	            end try
-	            begin catch
-		            select ERROR_NUMBER() as ErrorNumber;
-		            return -1;
-		            rollback transaction
-	            end catch 
-            end
+Select * from dbo.Release R
+inner join dbo.Release_Proyecto RP on RP.FKProyecto = @IDProyecto
+where R.id = RP.FKRelease
+commit transaction
+end try
+begin catch
+select ERROR_NUMBER() as ErrorNumber;
+return -1;
+rollback transaction
+end catch
+end
 
